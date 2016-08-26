cfbd8a6a3f68cce962219fa7f1633e5c8a0bad30
Jeletwilight <koonjj@gmail.com>
2016-08-26 16:22:51 +0700

    Complete HW 1-2

--- hw1-1/src/Hw1_1.java
+++ hw1-1/src/Hw1_1.java
@@ -4,10 +4,11 @@
 public class Hw1_1 {
     public int countEvenNum(int[] inputs)
     {
-        /**
-         * Your code here
-         */
-        return 0;
+        int nofeven = 0;
+        for(int i=0;i<inputs.length;i++){
+            if(inputs[i]%2 == 0)nofeven = nofeven+1;
+        }
+        return nofeven;
     }
 
 
