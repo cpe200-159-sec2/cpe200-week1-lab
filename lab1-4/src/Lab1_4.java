bab850d6fe2b9e7d5fdf8e34cefc850abeee7d9e
lalitaban <lalita.bandasak@gmail.com>
2016-08-26 16:04:33 +0700

    complete 1 2 3 4

--- lab1-4/src/Lab1_4.java
+++ lab1-4/src/Lab1_4.java
@@ -5,10 +5,16 @@ public class Lab1_4 {
 
     public float findAverage(int[] inputs)
     {
-        /*
-          Implement your code here
-        */
-        return 0.0f;
+        float sum=0;
+        float arv=0;
+
+       for(int i=0;i<inputs.length;i++){
+           sum += inputs[i];
+
+
+       }
+        arv=sum/inputs.length;
+       return arv;
     }
 
     public static void main(String[] args) {
