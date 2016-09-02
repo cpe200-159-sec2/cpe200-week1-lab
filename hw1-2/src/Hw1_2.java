538f2af928e59553677008459899fd5b0bdf8ff5
Panatchakorn <panatchakorn4774@gmail.com>
2016-09-02 10:38:00 +0700

    homeworks complete

--- hw1-2/src/Hw1_2.java
+++ hw1-2/src/Hw1_2.java
@@ -2,18 +2,26 @@
  * Created by pruet on 11/8/2559.
  */
 public class Hw1_2 {
-    float gpaCalculation(int[] credit, int[] grade)
-    {
-        /**
-         * your code here
-         */
-        return 0.0f;
-    }
-
     public static void main(String[] args) {
         int[] credit = {3, 3, 3, 3, 2};
         int[] grade  = {2, 4, 4, 3, 2};
         Hw1_2 h = new Hw1_2();
         System.out.printf("Your GPA is %f\n", h.gpaCalculation(credit, grade));
+    }
+
+    float gpaCalculation(int[] credit, int[] grade)
+    {
+        /**
+         * your code here
+         */
+        float sum=0;
+        int totalCredit=0;
+
+        for(int i=0;i<credit.length;i++){
+            sum += grade[i]*credit[i];
+
+            totalCredit += credit[i];
+        }
+        return sum/totalCredit;
     }
 }
